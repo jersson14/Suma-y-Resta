55e6c479076e7d5181edf5da5639e41dfbaf86e9
jersson14 <30710431+jersson14@users.noreply.github.com>
2018-12-05 15:28:30 -0500

    no message

---
+++ 1/1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numer1 = 14;
+            int numero2 = 12;
+
+            int total;
+
+            total = numer1 + numero2;
+            Console.WriteLine("el resultado es: " + total);
+            Console.ReadKey();
+        }
+    }
+}
